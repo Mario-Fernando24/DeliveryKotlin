75fcf4a93e2472862764e41054cbc2d9c1eb5caa
Mario-Fernando24 <3115408069.mafe@gmail.com>
2023-07-22 13:53:03 -0500

    style login

---
+++ app/src/main/java/com/mario/kotlindelivery/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mario.kotlindelivery.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mario.kotlindelivery.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/mario/kotlindelivery/activities/RegisterActivity.kt
+++ app/src/main/java/com/mario/kotlindelivery/activities/RegisterActivity.kt
@@ -1,11 +1,12 @@
-package com.mario.kotlindelivery
+package com.mario.kotlindelivery.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mario.kotlindelivery.R
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
